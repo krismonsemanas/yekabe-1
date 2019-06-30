eb452dfc00ebadcc9cabe9c4d91dc8dfc12e59c4
masariuman <masariuman@gmail.com>
2019-06-30 13:26:22 +0700

    absen all

--- app/Http/Controllers/AbsenGuruController.php
+++ app/Http/Controllers/AbsenGuruController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Guru;
 use App\Karyawan;
+use App\Murid;
+use App\Absen;
 use Auth;
+use Carbon\Carbon;
 
 
 class AbsenGuruController extends Controller
@@ -42,6 +45,30 @@ class AbsenGuruController extends Controller
     public function store(Request $request)
     {
         //
+        // dd($request->id_guru);
+        $guru = Guru::findOrFail($request->id_guru);
+        // $absen = new Absen;
+        // foreach( $request->status as $stats ) {
+            for($i=0;$i < count($request->status);$i++) {
+                // echo $request;
+                $absen = new Absen;
+                $absen->periode_id = $guru->periode_id;
+                $absen->kelas_id = $guru->kelas_id;
+                $absen->mapel_id = $guru->mapel_id;
+                $absen->karyawan_id = $guru->karyawan_id;
+                $absen->data_murid_id = $request->id_siswa[$i];
+                $absen->jadwal = date('Y-m-d H:i:s', strtotime('+7 hours'));
+                $absen->status = $request->status[$i];
+                $absen->keterangan = $request->keterangan[$i];
+                $absen->save();
+            }
+            return redirect('guru/absen/kelas/'.$guru->id)->with('new','Absen Hari Ini Telah Dilakukan.');
+            // dd($request);
+            // $user['created_at'] = new DateTime;
+            // $user['updated_at'] = $user['created_at'];
+        // }
+        // User::insert($users);
+
     }
 
     /**
@@ -87,5 +114,26 @@ class AbsenGuruController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function kelas($id)
+    {
+        //
+        $data['guru'] = Guru::findOrFail($id);
+        $data['absen'] = Absen::where('periode_id',$data['guru']->periode_id)->where('kelas_id',$data['guru']->kelas_id)->where('mapel_id',$data['guru']->mapel_id)->whereDate('jadwal','=',date('Y-m-d', strtotime('+7 hours')))->get();
+        // dd(date('Y-m-d H', strtotime('+7 hours')));
+        return view('beken.absen.kelas',$data);
+
+    }
+
+    public function now($id)
+    {
+        //
+        $data['guru'] = Guru::findOrFail($id);
+        // $data['kelas'] = Murid::where('periode_id',$data['guru']->periode_id)->where('kelas_id',$data['guru']->kelas_id)->where('mapel_id',$data['guru']->mapel_id)->orderBy('siswa_id.nama','asc')->get();
+        $data['kelas'] = Murid::join('data_murid','murid.siswa_id','=','data_murid.id')->where('periode_id',$data['guru']->periode_id)->where('kelas_id',$data['guru']->kelas_id)->where('mapel_id',$data['guru']->mapel_id)->orderBy('nama','asc')->get();
+        // dd($data['siswa'] = $data['kelas']->siswa_id->first());
+        // dd($data['kelas']);
+        return view('beken.absen.now',$data);
     }
 }

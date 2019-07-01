5bb2754b61a5a6a0c7a5aa5c28981ffc250c9625
masariuman <masariuman@gmail.com>
2019-07-01 19:04:22 +0700

    dashboard guru

--- app/Http/Controllers/DashboardGuruController.php
+++ app/Http/Controllers/DashboardGuruController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Periode;
+use App\Guru;
+use App\Jadwal;
+use App\Karyawan;
+use App\Murid;
 
 class DashboardGuruController extends Controller
 {
     public function index()
     {
-        return view('is_guru.dashboard');
+        $user_id = Auth::id();
+        $periode = Periode::all()->last();
+        $karyawan = Karyawan::where('id_login',$user_id)->first();
+        $mapel = Guru::where('periode_id',$periode->id)->where('karyawan_id',$karyawan->id)->pluck('mapel_id');
+        $data['countmapel'] = $mapel->count();
+        $kelas = Guru::where('periode_id',$periode->id)->where('karyawan_id',$karyawan->id)->pluck('kelas_id');
+        $data['countkelas'] = $kelas->count();
+        $data['jadwal'] = Jadwal::where('periode_id',$periode->id)->whereIn('mapel_id',$mapel)->whereIn('kelas_id',$kelas)->orderBy('hari','ASC')->get();
+        $murid = Murid::where('periode_id',$periode->id)->whereIn('mapel_id',$mapel)->whereIn('kelas_id',$kelas)->get();
+        $data['countmurid'] = $murid->count();
+        return view('is_guru.dashboard',$data);
     }
 }

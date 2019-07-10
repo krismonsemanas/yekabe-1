11f4abcc4bc55cd3defe7d4f613881c93b583ed2
masariuman <masariuman@gmail.com>
2019-07-10 08:46:11 +0700

    ganti ykb ke sicerdas

--- resources/views/tenpureto/beken/slice/header.blade.php
+++ resources/views/tenpureto/beken/slice/header.blade.php
@@ -2,9 +2,9 @@
     <!-- Logo -->
     <a href="/manage/dashboard" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>Y</b>KB</span>
+      <span class="logo-mini"><b>S</b>CD</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>YKB</b>School</span>
+      <span class="logo-lg"><b>SI</b>Cerdas</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
